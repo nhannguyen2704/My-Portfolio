5e5b0927b3692d921b9102f0e1c62561303f8edd
nhannguyen2704 <nhanvilla2011@gmail.com>
2026-03-28 17:35:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,55 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
+projects_db = {
+        1: {
+            "title" : "DỰ ÁN MY DASH BOARD",
+            "tech" : "Python",
+            "desc" : "Dự án này giúp người dùng quản lý được những công việc mà họ cần làm trong ngày một cách hiệu quả hơn",
+            "code_snippet" : """   
+            def cap_nhat_noi_dung(noi_dung_cu, noi_dung_moi):
+                ket_noi = sqlite3.connect("todo.db")
+                con_tro = ket_noi.cursor()
+                sql = "UPDATE cong_viec SET noi_dung = ? WHERE noi_dung = ?"
+                con_tro.execute(sql, (noi_dung_moi, noi_dung_cu))
+                ket_noi.commit()
+                ket_noi.close()""",
+            "image" : "images/logo_mydashboard.jpeg",
+        },
+        2: {
+            "title" : "DỰ ÁN MY PORTFOLIO",
+            "tech" : "HTML , Python",
+            "desc" : "Dự án này giúp tôi giới thiệu bản thân và những thành quả tôi đạt được",
+            "code_snippet" : """
+            <!doctype html>
+            <html lang="en" class="scroll-smooth">
+            <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>My Portfolio</title>
+                <script src="https://cdn.tailwindcss.com"></script>
+                <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+            </head>
+            </html>""",
+            "image" : "images/logo_myportfolio.jpeg",
+        },
+        3: {
+            "title" : "DỰ ÁN QUẢN LÝ BÁN HÀNG",
+            "tech" : "Python",
+            "desc" : "Dự án này giúp quản lý được những sản phẩm mà kho hàng còn lại và những sản phẩm đã bán ra một cách hiệu quả hơn cùng với lịch sử bán hàng chi tiết",
+            "code_snippet" : """
+            def hien_thi_du_lieu():
+            for item in tree.get_children():
+                tree.delete(item)
+            du_lieu = database.lay_ds_san_pham()
+            for row in du_lieu:
+                tree.insert('', tk.END, values=row)""",
+            "image" : "images/logo_qlbh.jpeg",
+        }
+    }
 @app.route('/')
+
 def home():
     my_name = "Nguyễn Thành Nhân"
     my_age = "15 tuổi"
@@ -56,26 +105,35 @@ def home():
         {
             'name' : "DỰ ÁN MY DASH BOARD",
             'description' : "My Dashboard là một dự án được thiết kế để giúp người dùng quản lý và theo dõi các hoạt động hàng ngày của mình một cách hiệu quả.",
+            'id' : 1,
         },
         {
             'name' : "DỰ ÁN MY PORTFOLIO",
             'description' : "My Portfolio là một dự án được thiết kế để giúp tôi giới thiệu bản thân và những thành quả tôi đạt được",
+            'id' : 2,
         },
         {
             'name' : "DỰ ÁN QUẢN LÝ BÁN HÀNG",
             'description' : "Dự án quản lý bán hàng là một hệ thống được thiết kế để giúp các doanh nghiệp quản lý và theo dõi các hoạt động bán hàng của mình một cách hiệu quả.",
+            'id' : 3,
         }
     ]
 
     return render_template('index.html', name=my_name, age=my_age, bio=my_bio, my_skills=skills, class_lap_trinh=lop_lap_trinh, projects=du_an)
 
-@app.route('/projects')
-def projects():
-    return "This is the projects page."
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route("/project/<int:project_id>")
+def project_detail(project_id):
+    project = projects_db.get(project_id)
+    if not project:
+        return "Dự án không tồn tại", 404
+    
+    return render_template('project_detail.html', p=project)
+    
 
 @app.route('/base')
 def base():
